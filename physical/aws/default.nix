640b94992bb7e51c97ceb7b7338b0043c99b2d93
John Lotoski <john.lotoski@iohk.io>
2020-03-27 16:19:56 -0400

    Adds {t3,m5,r5}.xlarge ec2 class

--- physical/aws/default.nix
+++ physical/aws/default.nix
@@ -5,6 +5,8 @@
   t2-large = import ./t2.large.nix;
   t2-xlarge = import ./t2.xlarge.nix;
 
+  t3-xlarge = import ./t3.xlarge.nix;
+
   t3a-nano = import ./t3a.nano.nix;
   t3a-small = import ./t3a.small.nix;
   t3a-medium = import ./t3a.medium.nix;
@@ -18,7 +20,11 @@
   c5-2xlarge = import ./c5.2xlarge.nix;
   c5-4xlarge = import ./c5.4xlarge.nix;
 
+  m5-xlarge = import ./m5.xlarge.nix;
+
   m5ad-xlarge = import ./m5ad.xlarge.nix;
+
+  r5-xlarge = import ./r5.xlarge.nix;
 
   r5a-xlarge = import ./r5a.xlarge.nix;
 
