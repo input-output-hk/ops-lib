e82f8a971d988ff1f4c85b189f241fd520702d8d
Samuel Evans-Powell <mail@sevanspowell.net>
2020-05-18 10:28:59 +0800

    Continue with custom overrides

--- modules/sentry/overrides.nix
+++ modules/sentry/overrides.nix
@@ -1,8 +1,53 @@
-{ pkgs }:
+{ pkgs, system ? builtins.currentSystem }:
 
 self: super:
 
-let
+{
+  amqp = self.buildPythonPackage rec {
+    pname = "amqp";
+    version = "1.4.9";
+    src = self.fetchPypi {
+      inherit pname version;
+      sha256 = "06n6q0kxhjnbfz3vn8x9yz09lwmn1xi9d6wxp31h5jbks0b4vsid";
+    };
+
+    checkInputs = [ self.mock self.coverage self.nose-cover3 self.unittest2 ];
+  };
+
+  django-crispy-forms = self.buildPythonPackage rec {
+    pname = "django-crispy-forms";
+    version = "1.6.1";
+    src = self.fetchPypi {
+      inherit pname version;
+      sha256 = "1h7rgzg8f6wr5a5xqhfy8awss86ijqfp8rvc4b3fc6hi9sjg7568";
+    };
+
+    doCheck = false;
+  };
+
+  email_reply_parser = self.buildPythonPackage rec {
+    pname = "email_reply_parser";
+    version = "0.2.0";
+    src = self.fetchPypi {
+      inherit pname version;
+      sha256 = "0rlhsvs1ii93fq2774dn45gwfqgwx1f4fv4k27v1zr0h1zj9il1z";
+    };
+    # RuntimeError: dictionary changed size during iteration
+    doCheck = false;
+  };
+
+  kombu = self.buildPythonPackage rec {
+    pname = "kombu";
+    version = "3.0.35";
+    src = self.fetchPypi {
+      inherit pname version;
+      sha256 = "09xpxpjz9nk8d14dj361dqdwyjwda3jlf1a7v6jif9wn2xm37ar2";
+    };
+
+    propagatedBuildInputs = [ self.amqp self.anyjson ];
+    checkInputs = [ self.mock self.unittest2 self.nose self.redis ];
+  };
+
   googleapis_common_protos = super.googleapis_common_protos.overrideAttrs ( oldAttrs: rec {
     pname = "googleapis-common-protos";
     version = "1.6.0";
@@ -11,10 +56,46 @@ let
       sha256 = "1im0ad5vdyjagy1hwp5xlw67l35i3griayvfgi46p5vbwgaqw6z6";
     };
     propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ self.protobuf ];
-    doCheck = false;
-    doInstallCheck = false;
   });
-in
-{
-  inherit googleapis_common_protos;
+
+  google_cloud_bigtable = super.google_cloud_bigtable.overrideAttrs ( oldAttrs: rec {
+    pname = "google-cloud-bigtable";
+    version = "0.32.2";
+    src = self.fetchPypi {
+      inherit pname version;
+      sha256 = "1gd857075xjxbb9b729qswd0a0gildrnw5z2s05zfa62160grla0";
+    };
+  });
+
+  mmh3 = self.buildPythonPackage rec {
+    pname = "mmh3";
+    version = "2.3.1";
+    src = self.fetchPypi {
+      inherit pname version;
+      sha256 = "0a795lk2gqj5ar0diwpd0gsgycv83pwlr0a91fki2ch9giaw7bgc";
+    };
+  };
+
+  parsimonious = super.parsimonious.overrideAttrs ( oldAttrs: rec {
+    pname = "parsimonious";
+    version = "0.8.0";
+    src = self.fetchPypi {
+      inherit pname version;
+      sha256 = "0sq81p00vsilvwyqpzp66vwbygp791bmyfii4hzp0mvf5bbnj25f";
+    };
+  });
+
+  # semaphore = self.buildPythonPackage rec {
+  #   pname = "semaphore";
+  #   version = "0.4.65";
+
+  #   src = self.fetchPypi {
+  #     inherit pname version;
+  #     sha256 = "0ly1v2ya4gm7qfam7r2vfnz5qdfl7khmjb1dklaca7spxzjwmfzw";
+  #     extension = "zip";
+  #   };
+
+  #   propagatedBuildInputs = [ self.milksnake ];
+  # };
+
 }
