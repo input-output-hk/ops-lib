4233dbd0bfa311195755bc456da862ba587698f0
Samuel Evans-Powell <mail@sevanspowell.net>
2020-05-18 09:14:59 +0800

    Refactor overriding code

--- modules/sentry/overrides.nix
+++ modules/sentry/overrides.nix
@@ -6,7 +6,10 @@ let
   googleapis_common_protos = super.googleapis_common_protos.overrideAttrs ( oldAttrs: rec {
     pname = "googleapis-common-protos";
     version = "1.6.0";
-    src = self.fetchPypi { inherit pname version; };
+    src = self.fetchPypi {
+      inherit pname version;
+      sha256 = "1im0ad5vdyjagy1hwp5xlw67l35i3griayvfgi46p5vbwgaqw6z6";
+    };
     propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ self.protobuf ];
     doCheck = false;
     doInstallCheck = false;
--- modules/sentry/release.nix
+++ modules/sentry/release.nix
@@ -6,11 +6,44 @@ let
   packageOverrides = pkgs.lib.foldr pkgs.lib.composeExtensions (self: super: {}) [overlay];
 
   py = pkgs.python.override { inherit packageOverrides; self = py; };
+
+  iohkMkPythonApplication = { python, overrides, ... }@attrs:
+    let
+      specialAttrs = [
+        "overrides"
+      ];
+      passedAttrs = builtins.removeAttrs attrs specialAttrs;
+
+      deps = map (depName: py.pkgs."${depName}") (builtins.attrNames (overrides {} {}));
+
+      packageOverrides = pkgs.lib.foldr pkgs.lib.composeExtensions (self: super: {}) [overrides];
+
+      py = python.override { inherit packageOverrides; self = py; };
+    in
+      python.pkgs.buildPythonApplication (
+        passedAttrs // {
+          propagatedBuildInputs = (attrs.propagatedBuildInputs or []) ++ deps;
+        }
+      );
 in
 {
   sentry = pkgs.callPackage ./. {
-    inherit (py.pkgs) buildPythonPackage fetchPypi;
-    python = py;
+    inherit (py.pkgs) buildPythonPackage fetchPypi googleapis_common_protos;
+  };
+
+  sentryNew = iohkMkPythonApplication rec {
+    pname   = "sentry";
+    version = "10.0.0";
+    format = "wheel";
+
+    src = python.pkgs.fetchPypi {
+      inherit pname version format;
+      sha256 = "2695ed1cf11e5afee92de04dc1868de9f4c49d5876c34579c935d9d6cf9fbb03";
+      python = "py27";
+    };
+
+    python = pkgs.python;
+    overrides = overlay;
   };
 
   inherit py pkgs;
