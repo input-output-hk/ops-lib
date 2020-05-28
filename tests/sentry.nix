21279770ffcfccd727837b17f70ebbc19cdebb89
Samuel Evans-Powell <mail@sevanspowell.net>
2020-05-28 17:15:40 +0800

    WIP module

--- tests/sentry.nix
+++ tests/sentry.nix
@@ -11,6 +11,13 @@
 
       virtualisation.memorySize = 2048;
       virtualisation.diskSize   = 2048;
+
+      # Postgres database
+      services.postgresql.enable = true;
+      services.postgresql.package = pkgs.postgresql_11;
+
+      # Redis
+      services.redis.enable = true;
     };
   };
 
