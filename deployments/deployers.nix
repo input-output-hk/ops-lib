837814ce92dcafc5d5f8fc7a5eca5f725e341204
Samuel Leathers <samuel.leathers@iohk.io>
2020-10-30 10:32:54 -0400

    bench-deployer: c5.4xlarge

--- deployments/deployers.nix
+++ deployments/deployers.nix
@@ -143,6 +143,7 @@ in {
   };
   bench-deployer = { pkgs, ... }: {
     local.username = "dev";
+    deployment.ec2.instanceType = "c5.4xlarge";
     deployment.ec2.ebsInitialRootDiskSize = pkgs.lib.mkForce 2000;
     users.users.dev = {
       openssh.authorizedKeys.keys = with pkgs.iohk-ops-lib.ssh-keys; allKeysFrom csl-developers;
