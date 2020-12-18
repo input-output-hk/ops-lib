07a9d53b7552a97cb088342930ecebaddfcb1330
Jonathan Ringer <jonathan.ringer@iohk.io>
2020-12-17 17:47:10 -0800

    zfs-image: export top-level attr

--- default.nix
+++ default.nix
@@ -3,6 +3,14 @@
 , config ? {}
 , sourcesOverride ? {}
 , pkgs ? import ./nix { inherit system crossSystem config sourcesOverride; } }:
+
+let
+  makeZfsImage = { module, type, system, ... }:
+    pkgs.lib.hydraJob ((import ("${pkgs.path}/nixos/lib/eval-config.nix") {
+      inherit system;
+      modules = [ module ];
+    }).config.system.build.zfsImage);
+in
 with pkgs; {
   inherit nixops nginxStable nginxMainline;
   overlays = import ./overlays sourcePaths;
@@ -10,5 +18,10 @@ with pkgs; {
     buildInputs = [ niv nixops nix telnet dnsutils ];
     NIX_PATH = "nixpkgs=${path}";
     NIXOPS_DEPLOYMENT = "${globals.deploymentName}";
+  };
+  zfs-image = makeZfsImage {
+    module = ./zfs/make-zfs-image.nix;
+    type = "zfs";
+    inherit system;
   };
 }
