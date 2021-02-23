3ed19eff6f09fa83e61f2621810605ca90bb8dd0
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2021-02-23 16:36:11 +0100

    Add memory and cpu info for aws instances.

--- physical/aws/common.nix
+++ physical/aws/common.nix
@@ -1,5 +1,6 @@
 { name, config, resources, pkgs, lib, ... }:
 let
+  inherit (pkgs) aws-instances;
   inherit (lib) mkDefault;
   inherit (config.deployment.ec2) region;
 
@@ -25,6 +26,8 @@ in {
   };
 
   node = {
+    cpus = aws-instances.${config.deployment.ec2.instanceType}.vCPU;
+    inherit (aws-instances.${config.deployment.ec2.instanceType}) memory;
     inherit accessKeyId region;
     fqdn = "${name}.${domain}";
   };
