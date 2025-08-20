7033494da83b8c8bae4af06d20ae4ecf4d6a00d8
John Lotoski <john.lotoski@iohk.io>
2025-08-19 19:00:23 -0500

    bump: nixpkgs -> 25.05, nix -> 2.29.1, iohk-nix, niv -> latest

--- nix/default.nix
+++ nix/default.nix
@@ -8,7 +8,7 @@ let
 
   iohkNix = import sourcePaths.iohk-nix {};
 
-  # use our own nixpkgs if it exists in our sources,
+  # Use our own nixpkgs if it exists in our sources,
   # otherwise use iohkNix default nixpkgs.
   nixpkgs = if (sourcePaths ? nixpkgs)
     then sourcePaths.nixpkgs
@@ -20,9 +20,7 @@ let
   [
     (import ../globals-deployers.nix)
     (final: prev: {
-      # A recent version of nix daemon is required to be able to migrate to newer nixpkgs, such as 25.05,
-      # otherwise an `error: path '/nix/store/...-linux-$VERSION-modules-shrunk/lib` is encountered during build.
-      nix = (builtins.getFlake "github:nixos/nix/9328af84d33281ef8018251b2a4289e89719c7ae").packages.${final.system}.nix;
+      nix = (flake-compat {inherit pkgs; src = sourcePaths.nixpkgs;}).defaultNix.legacyPackages.${final.system}.nixVersions.nix_2_29;
     })
   ];
 
